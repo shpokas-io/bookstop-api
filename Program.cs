904a6b41e378a7028463a7fbf41ea6a8ed90f990
shpokas <s.shpokas@gmail.com>
2024-11-09 15:53:49 +0200

    refactor: Move data seeding logic to Program.cs and remove redundancy in LibraryContext
    - Removed redundant data seeding in LibraryContext's OnModelCreating method.
    - Added conditional data seeding logic in Program.cs to populate the database with initial book data only if empty.
    - Simplified LibraryContext class by removing OnModelCreating method and redundant HasData setup.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using bookstopAPI.Data;
+using bookstopAPI.Models;
 using DotNetEnv;
 
 DotNetEnv.Env.Load();
@@ -10,5 +11,21 @@ builder.Services.ConfigureServices();
 var app = builder.Build();
 
 app.ConfigureMiddleware();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<LibraryContext>();
+    if (!context.Books.Any())
+    {
+        context.Books.AddRange(new List<Book>
+        {
+            new Book { Name = "Harry Potter and the Sorcerer's Stone", Year = 1997, Type = "Book", PictureUrl = "https://covers.openlibrary.org/b/id/7984916-L.jpg" },
+            new Book { Name = "Harry Potter and the Chamber of Secrets", Year = 1998, Type = "Book", PictureUrl = "https://covers.openlibrary.org/b/id/7984919-L.jpg" },
+            new Book { Name = "The Lord of the Rings: The Fellowship of the Ring", Year = 1954, Type = "Book", PictureUrl = "https://covers.openlibrary.org/b/id/6202464-L.jpg" },
+            new Book { Name = "The Lord of the Rings: The Two Towers", Year = 1954, Type = "Audiobook", PictureUrl = "https://covers.openlibrary.org/b/id/6202465-L.jpg" }
+        });
+        context.SaveChanges();
+    }
+}
 
 app.Run();
