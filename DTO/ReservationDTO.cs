26a4c593b0b881928e451005bf93bb261935a2d7
shpokas <s.shpokas@gmail.com>
2024-11-10 12:15:25 +0200

    fix: Add required properties `BookName` and `BookPictureUrl` to `ReservationDTO` and ensure correct mapping
    - Updated `ReservationDTO` to include `BookName` and `BookPictureUrl` as required properties.
    - Modified `CreateReservation` in `ReservationsController` to correctly map and assign `BookName` and `BookPictureUrl`.
    - Added additional validation to the reservation creation flow.
    - Added improved error handling to enhance debugging capabilities.

--- DTO/ReservationDTO.cs
+++ DTO/ReservationDTO.cs
@@ -7,7 +7,9 @@ namespace bookstopAPI.DTOs
     public required string UserId { get; set; }
     public int Days { get; set; }
     public bool IsAudiobook { get; set; }
-    public bool isQuickPickUp { get; set; }
+    public bool IsQuickPickUp { get; set; }
     public decimal TotalCost { get; set; }
+    public required string BookName {get; set;}
+    public required string BookPictureUrl {get; set;}
   }
 }
